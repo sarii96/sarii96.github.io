bc871fb80b41f49b4afa5bbc7e3a26e625a3f0fd
sarii96 <sara-96325@hotmail.com>
2020-02-01 17:13:58 -0700

    hi

---
+++ lesson4/scripts/scripts.js
@@ -0,0 +1,33 @@
+let currentDate = new Date();
+
+let year = currentDate.getFullYear();
+let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August', 
+    'September',
+    'October',
+    'November',
+    'December'
+]
+let daysofweek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+]
+let dayofweek = currentDate.getDay()
+let numberday = currentDate.getDate()
+
+let fullDate = daysofweek[dayofweek];
+fullDate += ", " + numberday + " " + months[currentDate.getMonth()] + " " + year;
+
+document.querySelector('#current-date').textContent = fullDate;
