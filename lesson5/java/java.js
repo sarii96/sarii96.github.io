9481d8724bbd6d16fbeaf9079ddc912bd9d8c95b
sarii96 <sara-96325@hotmail.com>
2020-02-07 10:03:08 -0700

    hi

--- lesson5/java/java.js
+++ lesson5/java/java.js
@@ -1,6 +1,8 @@
 let currentDate = new Date();
 
 let year = currentDate.getFullYear();
+
+
 let months = [
     'January',
     'February',
@@ -14,7 +16,7 @@ let months = [
     'October',
     'November',
     'December'
-]
+];
 let daysofweek = [
     'Sunday',
     'Monday',
@@ -23,25 +25,44 @@ let daysofweek = [
     'Thursday',
     'Friday',
     'Saturday'
-]
-let dayofweek = currentDate.getDay()
-let numberday = currentDate.getDate()
+];
+
+let dayofweek = currentDate.getDay();
+
+
+let numberday = currentDate.getDate();
 
 let fullDate = daysofweek[dayofweek];
+
+console.log(fullDate); //Thursday
+
 fullDate += ", " + numberday + " " + months[currentDate.getMonth()] + " " + year;
 
 document.querySelector('#current-date').textContent = fullDate;
 
+
+// -------------------------------------------------------------------------
+// -------------------------------------------------------------------------
+document.getElementById("current-temp").innerHTML= "22&deg;F";
+document.getElementById("wind-speed").innerHTML= "5 mph";
+document.getElementById("humidity").innerHTML= "93%";
+
+
+
+
+
 // -------------------------------------------------------------------------
 // -------------------------------------------------------------------------
 
-const hamButton =
-document.querySelector(".ham");
+
+const hamButton = document.querySelector(".ham");
+
 hamButton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
-
     document.querySelector(".navigation").classList.toggle("responsive");
 }
 
+// -------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 
