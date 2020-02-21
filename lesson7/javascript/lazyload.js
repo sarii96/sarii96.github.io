044bf5d1a708ce0460ce6df9db8b5581b6ad6bf7
sarii96 <sara-96325@hotmail.com>
2020-02-21 14:51:20 -0700

    hi

--- lesson7/javascript/lazyload.js
+++ lesson7/javascript/lazyload.js
@@ -6,6 +6,38 @@ const loadImages = (image) => {
   };
 };
 
+//------------------------------------------------------
+
+
+const imgOption = {
+    threshold: 9,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+
+//-----------------------------------------------------
+
+
+
+
 imagesToLoad.forEach((img) => {
     loadImages(img);
   });
