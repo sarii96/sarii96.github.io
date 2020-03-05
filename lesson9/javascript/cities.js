60f3a4927f17a3d68b5e3b00d796c0629e06d8a1
sarii96 <sara-96325@hotmail.com>
2020-03-05 12:06:28 -0700

    hi

--- lesson9/javascript/cities.js
+++ lesson9/javascript/cities.js
@@ -17,9 +17,24 @@ fetch('https://byui-cit230.github.io/weather/data/towndata.json')
              document.querySelector('#fishHavenImage').setAttribute('alt', town.name);
              
              } else if (town.name === 'Preston'){
+                document.querySelector('#prestonName').textContent = town.name;
+                document.querySelector('#prestonMotto').textContent = town.motto;
+                document.querySelector('#prestonYearFounded').textContent = `Year Founded: ${town.yearFounded}`;
+                document.querySelector('#prestonPopulation').textContent = `Population: ${town.currentPopulation}`;
+                document.querySelector('#prestonAnnualRainfall').textContent = `Annual Rainfall: ${town.averageRainfall}`;
+                document.querySelector('#prestonImage').setAttribute('src', `images/${town.photo}`);
+                document.querySelector('#prestonImage').setAttribute('alt', town.name);
+                
 
-              } else if (town.name === 'Soda Spring') {
-
+              } else if (town.name === 'Soda Springs') {
+                document.querySelector('#sodaSpringsName').textContent = town.name;
+                document.querySelector('#sodaSpringsMotto').textContent = town.motto;
+                document.querySelector('#sodaSpringsYearFounded').textContent = `Year Founded: ${town.yearFounded}`;
+                document.querySelector('#sodaSpringsPopulation').textContent = `Population: ${town.currentPopulation}`;
+                document.querySelector('#sodaSpringsAnnualRainfall').textContent = `Annual Rainfall: ${town.averageRainfall}`;
+                document.querySelector('#sodaSpringsImage').setAttribute('src', `images/${town.photo}`);
+                document.querySelector('#sodaSpringsImage').setAttribute('alt', town.name);
+                
               }
             }
         
