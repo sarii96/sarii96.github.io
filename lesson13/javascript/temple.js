1048fe0fd1e30497a08a03e477f5c0194cafe932
sarii96 <sara-96325@hotmail.com>
2020-04-03 00:05:36 -0600

    hi

---
+++ lesson13/javascript/temple.js
@@ -0,0 +1,52 @@
+
+const hamButton = document.querySelector("#ham");
+
+hamButton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+    document.querySelector(".navigation").classList.toggle("responsive");
+}
+
+//------------------------------------------------
+
+let currentDate = new Date();
+
+let year = currentDate.getFullYear();
+
+
+let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August', 
+    'September',
+    'October',
+    'November',
+    'December'
+];
+let daysofweek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+let dayofweek = currentDate.getDay();
+
+
+let numberday = currentDate.getDate();
+
+let fullDate = daysofweek[dayofweek];
+
+console.log(fullDate); //Thursday
+
+fullDate += ", " + numberday + " " + months[currentDate.getMonth()] + " " + year;
+
+document.querySelector('#current-date').textContent = fullDate;
